fcf06f3ed212cf0ce9dd51ae133adf2812aaa592
Pisut Hankongkaew <pisuthankongkaew@Pisuts-MacBook-Pro.local>
2018-03-15 18:13:11 +0700

    So far

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+
+const _get = require('lodash/get');
 const size = 10;
-const shipType = [4,3,3,2,2,2,1,1,1,1];
+const shipType = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
 
 function get() {
     let field = createField();
@@ -35,12 +37,10 @@ function place(type, field) {
     for (let i = 0; i < type; i++) {
         let x = stance == 0 ? startCordinate[0] + i : startCordinate[0];
         let y = stance == 0 ? startCordinate[0] : startCordinate[1] - i;
-        try {
-            let space = field[x][y];
-            if (space != 0) {
-                return false;
-            }
-        } catch (e) {
+        if (!canPlace(field, x, y)) {
+            return false;
+        }
+        if (isAdjacent(field, [x, y], type, stance, i)) {
             return false;
         }
         shipCodinate.push([x, y]);
@@ -53,6 +53,57 @@ function place(type, field) {
 
     return true;
 
+}
+function isAdjacent(field, cordinate, type, stance, order) {
+
+    let left = _get(field, `[${cordinate[0] - 1}][${cordinate[1]}]`)
+    let right = _get(field, `[${cordinate[0] + 1}][${cordinate[1]}]`)
+    let up = _get(field, `[${cordinate[0] - 1}][${cordinate[1] + 1}]`)
+    let down = _get(field, `[${cordinate[0] - 1}][${cordinate[1] - 1}]`)
+    let upLeft = _get(field, `[${cordinate[0] - 1}][${cordinate[1] + 1}]`)
+    let upright = _get(field, `[${cordinate[0] + 1}][${cordinate[1] + 1}]`)
+    let downLeft = _get(field, `[${cordinate[0] - 1}][${cordinate[1] - 1}]`)
+    let downRight = _get(field, `[${cordinate[0] + 1}][${cordinate[1] - 1}]`)
+    let isHead = order == 0;
+    let isTail = order == type - 1;
+    let isOccupied = (v) => v != undefined && v != 0;
+
+    if (type == 1) {
+        return isOccupied(left)
+            || isOccupied(right)
+            || isOccupied(up)
+            || isOccupied(down)
+            || isOccupied(upLeft)
+            || isOccupied(upright)
+            || isOccupied(downLeft)
+            || isOccupied(downRight);
+    } else if (type == 2) {
+        return (isTail && stance == 0 ? false : isOccupied(left))
+            || (isHead && stance == 0 ? false : isOccupied(right))
+            || (isTail && stance == 1 ? false : isOccupied(up))
+            || (isHead && stance == 1 ? false : isOccupied(down))
+            || isOccupied(upLeft)
+            || isOccupied(upright)
+            || isOccupied(downLeft)
+            || isOccupied(downRight);
+    } else if (type == 3) {
+        return false;
+    } else if (type == 4) {
+        return false;
+    }
+
+
+}
+function canPlace(field, x, y) {
+    try {
+        let space = field[x][y];
+        if (space != 0) {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 function random(max) {
